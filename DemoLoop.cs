cd00a98a39e745b9484353b9235189105a41a6d2
Chaitra P1 <chaitra.p1@ibm.com>
2022-05-21 08:22:01 +0530

    First program in C#

---
+++ DemoLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public  class DemoLoop
+    {
+
+        public static void PrintForLoop()
+        {
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine("Printing this line 5 times");
+            }
+
+        }
+    }
+}
